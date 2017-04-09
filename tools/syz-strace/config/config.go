c5f004cce69e37b825b193b4cc627ebd1c03cb27
shankarapailoor <sp3485@gmail.com>
2017-04-09 16:35:54 -0400

    adding config and tracer

---
+++ tools/syz-strace/config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"io/ioutil"
+	"github.com/Sirupsen/logrus"
+	"encoding/json"
+)
+
+type DistillConfig struct {
+	CorpusGenConf CorpusGenConfig `json:"corpus_gen_conf"`
+	InpIOConf InputIOConfig `json:"io"`
+	DistillEnabled bool `json:"distill_enabled"`
+}
+
+type CorpusGenConfig struct {
+	ConfigPath string `json:"workload_config"`
+	Ip string
+	Port int
+	KeyFile string `json:"ssh_key"`
+}
+
+type InputIOConfig struct {
+	InpType string `json:"type"`
+	LocalConfig
+	GceConf GceConfig
+}
+
+type LocalConfig struct {
+	InputDirectory string
+	Files []string
+	Filter []string
+	OutputDirectory string
+}
+
+type GceConfig struct {
+
+}
+
+func NewConfig(location string) (config *DistillConfig) {
+	dat, fileErr := ioutil.ReadFile(location)
+	if fileErr != nil {
+		logrus.Fatalf("Unable to read config, exiting")
+	}
+	if err := json.Unmarshal(dat, &config); err != nil {
+		logrus.Fatalf("Unable to read config: %s", err.Error())
+	}
+	return
+}
---
+++ tools/syz-strace/workload-tracer/tracer.go
@@ -0,0 +1,123 @@
+package workload_tracer
+
+import (
+	"io/ioutil"
+	"github.com/Sirupsen/logrus"
+	"encoding/json"
+	"os/exec"
+	"cloud.google.com/go/storage"
+	"golang.org/x/net/context"
+	. "github.com/google/syzkaller/tools/syz-strace/ssh"
+	"github.com/google/syzkaller/tools/syz-strace/config"
+)
+
+type WorkloadConfig struct {
+	ExecutablePath string
+	Iterations int
+	FollowFork bool
+	MultiThreaded bool
+	Exec bool
+	Args []string
+	StraceOutPath string
+	KcovOutPath string
+	Name string
+}
+
+func write(client *storage.Client, bucket, objectName string, data []byte) error {
+	ctx := context.Background()
+	// [START upload_file]
+	wc := client.Bucket(bucket).Object(objectName).NewWriter(ctx)
+
+	if _, err := wc.Write(data); err != nil {
+		return err
+	}
+	if err := wc.Close(); err != nil {
+		return err
+	}
+	// [END upload_file]
+	return nil
+}
+
+func create(client *storage.Client, projectId, bucketName string) error {
+	ctx := context.Background()
+	attrs := &storage.BucketAttrs{
+		StorageClass: "MULTI_REGIONAL",
+	}
+	return client.Bucket(bucketName).Create(ctx, projectId, attrs)
+}
+
+func GenerateCorpus(genConfig config.CorpusGenConfig) (err error) {
+	//ctx := context.Background()
+	//client, err := storage.NewClient(ctx)
+	client := NewClient(genConfig)
+	if err != nil {
+		logrus.Fatalf("Unable to generate client config: %s", err.Error())
+	}
+	wcs := readWorkload(genConfig.ConfigPath)
+	for _, wc := range wcs {
+		RunStrace(wc, client)
+		//DeleteOutFile(wc)
+	}
+	return
+}
+
+func RunStrace(wc WorkloadConfig, client *SSHClient) error{
+	var err error
+	straceCmd := buildStraceCmd(wc)
+	if err = client.RunCommand(straceCmd); err != nil {
+		logrus.Fatalf("Failed to run prog: %s", wc.ExecutablePath)
+	}
+	return err
+}
+
+func DeleteOutFile(config WorkloadConfig) {
+	deleteCmd := exec.Cmd{}
+	deleteCmd.Path = "/bin/rm"
+	deleteCmd.Args = append([]string{deleteCmd.Path}, "-f")
+	deleteCmd.Args = append(deleteCmd.Args, config.StraceOutPath)
+	if err := deleteCmd.Run(); err != nil {
+		logrus.Fatalf("Failed to delete output file: %s", err.Error())
+	}
+	return
+}
+
+func readWorkload(location string) (wcs []WorkloadConfig) {
+	data, fileErr := ioutil.ReadFile(location)
+	if fileErr != nil {
+		logrus.Fatalf("Unable to read config, exiting")
+	}
+	if err := json.Unmarshal(data, &wcs); err != nil {
+		logrus.Fatalf("Unable to read config")
+	}
+	return
+}
+
+func buildStraceCmd(config WorkloadConfig) (sshCommand *SSHCommand) {
+	/*
+	straceCmd := exec.Cmd{}
+	straceCmd.Path = "/root/strace" //Add to config
+	straceCmd.Args = append([]string{straceCmd.Path}, "-s")
+	straceCmd.Args = append(straceCmd.Args, "65500")
+	straceCmd.Args = append(straceCmd.Args, "-o")
+	straceCmd.Args = append(straceCmd.Args, config.StraceOutPath)
+	straceCmd.Args = append(straceCmd.Args, "-k")
+	if config.FollowFork {
+		straceCmd.Args = append(straceCmd.Args, "-ff")
+	}
+	straceCmd.Args = append(straceCmd.Args, config.ExecutablePath)
+	return straceCmd
+	*/
+	sshCommand.Path = config.ExecutablePath
+	sshCommand.Args = append([]string{config.ExecutablePath}, "-s")
+	sshCommand.Args = append(sshCommand.Args, "65500")
+	sshCommand.Args = append(sshCommand.Args, "-o")
+	sshCommand.Args = append(sshCommand.Args, config.StraceOutPath)
+	sshCommand.Args = append(sshCommand.Args, "-k")
+	if config.FollowFork {
+		sshCommand.Args = append(sshCommand.Args, "-f")
+	}
+	sshCommand.Args = append(sshCommand.Args, config.ExecutablePath)
+	sshCommand.Args = append(sshCommand.Args, config.Args...)
+	return
+}
+
