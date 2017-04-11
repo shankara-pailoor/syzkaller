3f144ce23ba07d9d088d976a96070bdc9d0a5f5b
Shankara Pailoor <sp3485@columbia.edu>
2017-04-10 20:54:14 -0400

    sorting progs

--- tools/syz-strace/distiller/distiller.go
+++ tools/syz-strace/distiller/distiller.go
@@ -9,6 +9,7 @@ import (
 
 type Distiller interface {
 	MinCover(domain.Seeds) *prog.Prog
+	Contributes(domain.Seed, map[uint64]bool) int
 }
 
 type DefaultDistiller struct {
@@ -16,17 +17,28 @@ type DefaultDistiller struct {
 }
 
 func (d *DefaultDistiller) MinCover(seeds domain.Seeds) *prog.Prog {
-	fmt.Printf("Computing Min Cover with %d seeds", len(seeds))
+	fmt.Printf("Computing Min Cover with %d seeds\n", len(seeds))
 	seenIps := make(map[uint64]bool)
-	sort.Reverse(seeds)
+	sort.Sort(sort.Reverse(seeds))
 	contributing_progs := 0
 	for _, seed := range seeds {
-		var ips int = seed.Contributes(seenIps)
+		var ips int = d.Contributes(seed, seenIps)
 		if ips > 0 {
-			fmt.Printf("Seed: %s contributes: %d ips", seed.Call.Meta.Name, ips)
+			fmt.Printf("Seed: %s contributes: %d ips out of its total of: %d\n", seed.Call.Meta.Name, ips, len(seed.Cover))
 			contributing_progs += 1
 		}
 	}
 	fmt.Printf("Total Contributing: %d, out of %d", contributing_progs, len(seeds))
 	return nil
+}
+
+func (d *DefaultDistiller) Contributes(seed *domain.Seed, seenIps map[uint64]bool) int {
+	total := 0
+	for _, ip := range seed.Cover {
+		if _, ok := seenIps[ip]; !ok {
+			seenIps[ip] = true
+			total += 1
+		}
+	}
+	return total
 }
--- tools/syz-strace/domain/seeds.go
+++ tools/syz-strace/domain/seeds.go
@@ -25,18 +25,8 @@ func (s Seeds) Less(i, j int) bool {
 	return len(s[i].Cover) < len(s[j].Cover)
 }
 
-func (s Seeds) Add(seed *Seed) {
-	s = append(s, seed)
-}
-
-func (s *Seed) Contributes(seenIps map[uint64]bool) (total int) {
-	total = 0
-	for _, ip := range s.Cover {
-		if _, ok := seenIps[ip]; !ok {
-			total += 1
-		}
-	}
-	return total
+func (s *Seeds) Add(seed *Seed) {
+	*s = append(*s, seed)
 }
 
 func NewSeed(call *prog.Call, prog *prog.Prog, idx int, cover []uint64) *Seed{
