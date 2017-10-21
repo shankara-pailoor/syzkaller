0da2db15ca2dcfc9212b9612e26bc99a6b2f4d02
shankarapailoor <shankarapailoor@gmail.com>
2017-10-20 23:37:49 -0400

    executor taking directory of configs

--- tools/syz-strace/workload-tracer/tracer.go
+++ tools/syz-strace/workload-tracer/tracer.go
@@ -7,6 +7,8 @@ import (
 	"github.com/Sirupsen/logrus"
 	"encoding/json"
 	. "github.com/google/syzkaller/tools/syz-strace/domain"
+	"os"
+	"strings"
 )
 
 
@@ -27,12 +29,35 @@ func NewTracer(config config.CorpusGenConfig) (tracer Tracer) {
 }
 
 func readWorkload(location string) (wcs []WorkloadConfig) {
-	data, fileErr := ioutil.ReadFile(location)
-	if fileErr != nil {
-		logrus.Fatalf("Unable to read config, exiting")
+	reader := func (file os.FileInfo) {
+		tmpWcs := make([]WorkloadConfig, 0)
+		if strings.Contains(file.Name(), ".json") {
+			data, fileErr := ioutil.ReadFile(location)
+			if fileErr != nil {
+				logrus.Fatalf("Unable to read config, exiting")
+			}
+			if err := json.Unmarshal(data, &tmpWcs); err != nil {
+				logrus.Fatalf("Unable to read config")
+			}
+			wcs = append(wcs, tmpWcs...)
+		}
 	}
-	if err := json.Unmarshal(data, &wcs); err != nil {
-		logrus.Fatalf("Unable to read config")
+	finfo, err := os.Stat(location)
+	if err != nil {
+		panic(err.Error())
 	}
+	switch finfo.Mode() {
+	case os.ModeDir:
+		var finfos []os.FileInfo
+		if finfos, err = ioutil.ReadDir(location); err != nil {
+			panic(err.Error())
+		}
+		for _, file := range finfos {
+			reader(file)
+		}
+	default:
+		reader(finfo)
+	}
+
 	return
 }
